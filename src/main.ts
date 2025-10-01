e425ff36bfc06bd3158ebc79ae84deeb0e74dd09
Brayan-Uwu-Onichan <Panchomancorre90@gmail.com>
2025-09-30 19:32:53 -0500

    Fase 3 Completa: Swagger UI implementado y pruebas en Postman

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // <-- IMPORTA ESTO
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,17 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // --- INICIA CONFIGURACIÓN DE SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('La API para nuestro proyecto de gestión de tareas')
+    .setVersion('1.0')
+    .addTag('tasks') // Agruparemos nuestros endpoints bajo la etiqueta "tasks"
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // La URL será http://localhost:3000/api-docs
+  // --- TERMINA CONFIGURACIÓN DE SWAGGER ---
 
   await app.listen(3000);
 }
